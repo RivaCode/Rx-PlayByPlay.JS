28cc69f6438d267fd3a307ecf89954618ae6fc38
Stas Rivkin <stasr@codevalue.net>
2017-10-28 17:06:54 +0300

    initial impl. of text change in rx

--- LiveDemo/src/app/pages/googlesearch/googlesearch.component.ts
+++ LiveDemo/src/app/pages/googlesearch/googlesearch.component.ts
@@ -1,12 +1,36 @@
-import {Component, OnInit} from "@angular/core";
+import {
+  Component,
+  AfterContentInit,
+  ViewChild,
+  ElementRef
+} from "@angular/core";
+
+import {Observable} from "rxjs/Observable";
+import "rxjs/add/observable/fromEvent";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/filter";
+import "rxjs/add/operator/debounceTime";
+import "rxjs/add/operator/distinctUntilChanged";
+import "rxjs/add/operator/switchMap";
 
 @Component({
   selector: "app-googlesearch",
   templateUrl: "./googlesearch.component.html",
   styleUrls: ["./googlesearch.component.css"]
 })
-export class GooglesearchComponent implements OnInit {
+export class GooglesearchComponent implements AfterContentInit {
+  results: string[] = [];
+  @ViewChild("text") txb: ElementRef;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngAfterContentInit(): void {
+    Observable.fromEvent(this.txb.nativeElement, "keyup")
+      .map(_ => <string>this.txb.nativeElement.value)
+      .filter(candidate => candidate.length > 2)
+      .debounceTime(500)
+      .distinctUntilChanged()
+      .switchMap(token => Promise.resolve(["a", "b"].concat(token)))
+      .subscribe(results => (this.results = results));
+  }
 }
